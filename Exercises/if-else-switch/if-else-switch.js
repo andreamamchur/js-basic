87e4765e11e9c118f742822980e227da4ac27b64
Andrey Mamchur <a.s.mamchur@gmail.com>
2019-10-24 19:42:08 +0300

    HW5 if-else-switch task

--- Exercises/if-else-switch/if-else-switch.js
+++ Exercises/if-else-switch/if-else-switch.js
@@ -4,11 +4,47 @@ const codeToMissingComplexityElement = {
   2: 'special character',
   3: 'cannot be empty'
 };
+let specialCharacterCheck = false;
+const specialCharacters = ['!', '_', '-', '%', '(', ')', '#', '*', '{', '}'];
+let arrayResult = [];
 
 function getMissingComplexityElementCode(password) {
+  for (const char of password) {
+    if (specialCharacters.includes(char)) {
+      specialCharacterCheck = true;
+    }
+  }
 
+  if (password.length < 8) {
+    arrayResult.push(0);
+  }
+  if (password === password.toLowerCase()) {
+    arrayResult.push(1);
+  }
+  if (!specialCharacterCheck) {
+    arrayResult.push(2);
+  }
+  if (password.length === 0) {
+    arrayResult.push(3);
+  }
 }
 
-function getErrorMessage() {
-  return false;
+function getErrorMessage(array) {
+  for (const char of array) {
+    // eslint-disable-next-line default-case
+    switch (char) {
+    case 0:
+      console.log('The lenght of the password is less than 8 characters');
+      break;
+    case 1:
+      console.log('Password does not contain capital letter');
+      break;
+    case 2:
+      console.log('Password does not contain special characters');
+      break;
+    case 3:
+      console.log('Password can\'t be empty');
+      break;
+    }
+  }
 }
