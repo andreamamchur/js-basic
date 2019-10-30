40f76922d52ba04b685df566fb70d282032e90ab
Andrey Mamchur <a.s.mamchur@gmail.com>
2019-10-30 13:47:15 +0200

    HW3 Strings task updated according comments

--- Exercises/strings/strings.js
+++ Exercises/strings/strings.js
@@ -20,8 +20,9 @@ alert(`My favourite animal is ${myAnimal}`);
 const favouriteWriter = 'theodore dreiser';
 
 function toCapitalize(string) {
-    const index = string.indexOf(' ');
-    return string[0].toUpperCase() + string.slice(1, index + 1) + string[index + 1].toUpperCase() + string.slice(index + 2);
+  const index = string.indexOf(' ');
+  //return string[0].toUpperCase() + string.slice(1, index + 1) + string[index + 1].toUpperCase() + string.slice(index + 2);
+  return `${string[0].toUpperCase()}${string.slice(1, index + 1)}${string[index + 1].toUpperCase()}${string.slice(index + 2)}`;
 }
 
 //Declare a function which will receive password string. Check is password strong enough:
@@ -30,47 +31,48 @@ function toCapitalize(string) {
 // should contain at leas one special character.
 
 function passwordCheck(password) {
-    const specialCharacters = ['!', '_', '-', '%', '(', ')', '#', '*', '{', '}'];
-    let specialCharacterCheck = false;
+  const specialCharacters = ['!', '_', '-', '%', '(', ')', '#', '*', '{', '}'];
+  let specialCharacterCheck = false;
 
-    for (const char of password) {
-        if (specialCharacters.includes(char)) {
-            specialCharacterCheck = true;
-        }
+  for (const char of specialCharacters) {
+    if (password.includes(char)) {
+      specialCharacterCheck = true;
     }
-    if (password !== password.toLowerCase() && password.length > 7 && specialCharacterCheck) {
-        return 'Valid password';
-    }
+  }
+  if (password !== password.toLowerCase() && password.length > 7 && specialCharacterCheck) {
+    return 'Valid password';
+  }
 
-    return 'Invalid password';
+  return 'Invalid password';
 }
 
 //Make function which will generate password suggestion based on passed password, and required strongest.
 
 function passwordGeneration(password) {
-    let newPassword = password;
-    const specialCharacters = ['!', '_', '-', '%', '(', ')', '#', '*', '{', '}'];
-    let specialCharacterCheck = false;
+  let newPassword = password;
+  const specialCharacters = ['!', '_', '-', '%', '(', ')', '#', '*', '{', '}'];
+  let specialCharacterCheck = false;
 
-    for (const char of password) {
-        if (specialCharacters.includes(char)) {
-            specialCharacterCheck = true;
-        }
+  for (const char of password) {
+    if (specialCharacters.includes(char)) {
+      specialCharacterCheck = true;
     }
+  }
 
-    if (!specialCharacterCheck) {
-        newPassword += specialCharacters[Math.round(Math.random() * specialCharacters.length)];
+  if (!specialCharacterCheck) {
+    newPassword += specialCharacters[Math.round(Math.random() * specialCharacters.length)];
+  }
+
+  if (password === password.toLowerCase()) {
+    const index = Math.floor(Math.random() * password.length);
+    newPassword = password.substr(0, index) + password[index].toUpperCase() + password.slice(index + 1);
+  }
+
+  if (password.length < 8) {
+    for (let i = 0; i < (8 - password.length); i++) {
+      newPassword += String.fromCodePoint(65 + Math.round(Math.random() * 26));
     }
+  }
 
-    if (password === password.toLowerCase()) {
-        newPassword = password[0].toUpperCase() + password.slice(1);
-    }
-
-    if (password.length < 8) {
-        for (let i = 0; i < (8 - password.length); i++) {
-            newPassword += String.fromCodePoint(65 + Math.round(Math.random() * 26));
-        }
-    }
-
-    return newPassword;
+  return newPassword;
 }
