7f80949e2632a0ec7f85ef041da88647322bcda9
Andrey Mamchur <a.s.mamchur@gmail.com>
2019-10-30 17:04:47 +0200

    HW4 operators-conversion task updated according to comments

--- Exercises/operators-and-type-conversions/operators-and-type-conversions.js
+++ Exercises/operators-and-type-conversions/operators-and-type-conversions.js
@@ -1,6 +1,7 @@
 // Get `100` using `+` operator and numeric value `10`.
 
-const numberNew = +(10 + '0');
+//const numberNew = +(10 + '0');
+const numberNew = Number(10 + '0');
 
 // You have following string: `Amazon annual revenue of US$232.887 billion` extract revenue and count monthly income.
 
@@ -10,8 +11,7 @@ const revenueMonth = parseFloat(string.slice(string.indexOf('$') + 1)) / 12;
 // Declare variable which will contain logs counter. Log few times with updating this value accordingly.
 
 let counter = 0;
-counter++;
-console.log(counter);
-++counter;
-console.log(counter);
+
+console.log(++counter);
+console.log(++counter);
 console.log(++counter);
